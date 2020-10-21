f65b37ab1c64fb6a512dbe12ad26fcfdee675bbc
Sujan Paudel <sujan2000paudel@gmail.com>
2020-10-21 09:43:42 +0545

    Add Change in navbar and home section

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let pageNav = document.getElementById('pages');
+let pageCard = document.getElementById('page-card');
+let shopNav = document.getElementById('shop');
+let shopCard = document.getElementById('shop-card');
+
+function displayPage() {
+  pageCard.style.display = 'block';
+}
+
+function removePage() {
+  pageCard.style.display = 'none';
+}
+
+function displayShop() {
+  shopCard.style.display = 'flex';
+}
+
+function removeShop() {
+  shopCard.style.display = 'none';
+}
+
+pageCard.addEventListener('mouseenter', displayPage);
+pageCard.addEventListener('mouseleave', removePage);
+
+pageNav.addEventListener('mouseenter', displayPage);
+pageNav.addEventListener('mouseleave', removePage);
+
+shopCard.addEventListener('mouseenter', displayShop);
+shopCard.addEventListener('mouseleave', removeShop);
+
+shopNav.addEventListener('mouseenter', displayShop);
+shopNav.addEventListener('mouseleave', removeShop);
